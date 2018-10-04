507162c79de33d0e941de8e205f9e7d0294c2676
Dwayne <driv91197@gmail.com>
2018-10-04 12:19:12 -0400

    Added bootstrap, jquery, and popper. Updated index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,31 @@
 //Module management. Need to link math.js to index.js
 const math = require('./math');
 
-var sum = math.add(1,2);
+//Adding in express through a a constant, to be able to use express
+const express = require('express');
 
-console.log("1+2="+sum);
+
+
+console.log("2 + 2 = "+sum);
+
+//App for our server... express can be a function
+const app = express();
+
+app.get("/add", function(req, res, next){
+
+    var sum = math.add(2,2);
+    res.send({sum: sum});
+
+});
+
+//Maps function and handles request
+//Everytime user hits this location in the server, this will run
+//The "/" is the root location
+app.use("/", function(req, res, next){
+
+    //"Sending" information to the client. Like return
+    res.send("Hello World");
+
+});
+
+app.listen(3000);
