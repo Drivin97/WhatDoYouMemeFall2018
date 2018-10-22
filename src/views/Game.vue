bdafdc368e67b6ea52cf6fd398ccfe7d976c2354
Dwayne <driv91197@gmail.com>
2018-10-22 12:06:54 -0400

    Nothing is working

---
+++ src/views/Game.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="alert alert-success">
+        We have a game!
+    </div>
+</template>
+
+<style lang="sass">
+
+</style>
+
+<script>
+export default {
+    
+}
+</script>
