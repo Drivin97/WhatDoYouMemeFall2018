2dcb57cc36cf9e12786dccbccc659760920f330a
Dwayne <driv91197@gmail.com>
2018-10-29 11:56:30 -0400

    have all pages showing and updated code to follow in class

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,13 @@
 </template>
 
 <style lang="scss">
-  @import "../node_modules/bootstrap/scss/bootstrap.scss";
-  #logo {height: 50px;}
+@import "../node_modules/bootstrap/scss/bootstrap.scss";
+#logo { height: 50px;}
 </style>
 
 <script>
 // @ is an alias to /src
 import Nav from '@/components/Nav.vue'
-
 export default {
   name: 'home',
   components: {
--- src/services/api_access.js
+++ src/services/api_access.js
@@ -1,32 +1,33 @@
 const api_root = "http://localhost:80/game";
 
 export function GetState(){
-    return fetch(api_root + "/")
-            .then(function(response) {
-                return response.json();
-            });
+    return myFetch(api_root + "/")
 }
 export function FlipPicture(){
-    return postData(api_root + "/picture", {})
+    return myFetch(api_root + "/picture", {})
             
 }
 
 
-
-
-
-  function postData(url = ``, data = null) {
+  function myFetch(url = ``, data = null) {
+      
     // Default options are marked with *
-      return fetch(url, {
-          method:  "POST", // *GET, POST, PUT, DELETE, etc.
-          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-          credentials: "same-origin", // include, same-origin, *omit
-          headers: {
-              "Content-Type": "application/json; charset=utf-8",
-              // "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: JSON.stringify(data), // body data type must match "Content-Type" header
-      })
+      let options = {
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "include", // include, same-origin, *omit     
+      };
+      if(data) {
+            options = { 
+              ...options,
+              method:  "POST", // *GET, POST, PUT, DELETE, etc.
+              headers: {
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+        };
+      }
+      return fetch(url, options)
       .then(response =>{
         return response.json()
 
