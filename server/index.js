3b3760196bf5f0595ddd7b28e96ff2ac40e7be0b
Dwayne <driv91197@gmail.com>
2018-11-19 11:22:03 -0500

    Updated index.js

--- server/index.js
+++ server/index.js
@@ -10,8 +10,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
     next();
-});
-
+  });
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use("/", express.static(__dirname + "/../dist/"));
